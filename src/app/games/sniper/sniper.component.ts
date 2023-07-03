1a93c9930f83d74fc72b28967450bceda704e9cc
wevioz <wevioz@mail.ru>
2023-07-03 10:29:38 +0300

    Done the games part

--- src/app/games/sniper/sniper.component.ts
+++ src/app/games/sniper/sniper.component.ts
@@ -1,26 +1,66 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {GamesService} from "../games.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sniper',
   templateUrl: './sniper.component.html',
   styleUrls: ['./sniper.component.scss']
 })
-export class SniperComponent implements OnInit{
-  field: Element = document.querySelector(".sniper__field")!;
+export class SniperComponent implements OnInit {
+  field!: HTMLElement;
+  xSize!: number;
+  ySize!: number;
+  score: number = 0;
 
-  constructor(private element: ElementRef) {
-    console.log(this.field);
+  constructor(
+    private gamesService: GamesService,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
+    this.field = document.querySelector(".sniper__field")!;
+
+    this.xSize = +this.field.style.width;
+    this.ySize = +this.field.style.height;
+
     this.createBaloon();
   }
 
-  createBaloon() {
-    console.log(this.field)
-    const xSize = this.field.getAttribute("width");
-    const ySize = this.field.getAttribute("height");
+  getRandomArbitrary(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
-    console.log(xSize, ySize)
+  createBaloon() {
+    const baloon = document.createElement("div");
+    const size: number = this.getRandomArbitrary(50, 300);
+
+    baloon.style.setProperty("width", size + "px");
+    baloon.style.setProperty("height", size + "px");
+    baloon.style.setProperty("background-color", "#C7EEFF");
+    baloon.style.setProperty("border-radius", "50%");
+    baloon.style.setProperty("margin-top", this.getRandomArbitrary(0, 500 - size) + "px");
+    baloon.style.setProperty("margin-left", this.getRandomArbitrary(0, 1200 - size) + "px");
+
+    baloon.addEventListener("click", () => {
+      this.pop();
+    })
+
+    this.field.appendChild(baloon);
+  }
+
+  pop() {
+    this.field.innerHTML = "";
+
+    this.score += 5;
+
+    this.createBaloon();
+  }
+
+  finish() {
+    this.gamesService.replenish(this.score).subscribe(() => {
+      this.router.navigate(["/user/info"])
+    });
   }
 }
